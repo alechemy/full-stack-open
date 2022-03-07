4f6bf69457690d21d6ea27013a3b5059de1848fc
Alec Custer <alcuster@icloud.com>
2022-03-07 08:12:15 -0800

    pull resolvers and schema into own files

---
+++ exercises/part8/library-server/schema.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Query {
+    authorCount: Int!
+    bookCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      genres: [String!]!
+      published: Int!
+    ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
+  }
+`;
+
+module.exports = typeDefs;
